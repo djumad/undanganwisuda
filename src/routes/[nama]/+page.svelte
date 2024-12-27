1ef62ec41eb7affaaada171ea3b429c79bd35c41
djumad <bantandjumad@gmail.com>
2024-12-27 16:07:18 +0900

    Upgrade

--- src/routes/[nama]/+page.svelte
+++ src/routes/[nama]/+page.svelte
@@ -57,12 +57,10 @@
       <!-- Peta Google Maps -->
       <div class="w-full h-96">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d178.54987808926893!2d128.27738860091785!3d-3.612832889667739!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d6cedb2bf5f3f2b%3A0x3e82961c9b2a530d!2sHOREB%20PULSA!5e1!3m2!1sid!2sid!4v1735269941051!5m2!1sid!2sid"
-          style="border:0;"
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-          class="w-full h-full rounded-lg shadow-lg"
-        ></iframe>
+  src="https://www.google.com/maps/embed?pb=..."
+  style="border:0;"
+  title="Peta Lokasi Horeb Pulsa"
+></iframe>
       </div>
   
       <!-- Info Tambahan -->
